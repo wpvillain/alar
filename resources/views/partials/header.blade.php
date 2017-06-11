2375c1b9b43ea6b379afac1bcb890d3c2ef0353d
Jasper Frumau <jasper@imagewize.com>
2017-06-11 07:31:57 +0300

    made navbar cover full width with items centered

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,20 +1,18 @@
 <header class="banner">
- <div class="container">
+ 
  <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
-<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
- data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
- aria-label="Toggle navigation">
- <span class="navbar-toggler-icon"></span>
- </button>
-<a class="navbar-brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-<div class="collapse navbar-collapse" id="navbarSupportedContent">
- @if (has_nav_menu('primary_navigation'))
- {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'navbar-nav nav pull-right']) !!}
- @endif
- </div>
- <!-- <form class="form-inline collapse navbar-collapse pull-right">
- <button class="btn btn-outline-success" type="button">Main button</button>
- </form> -->
+	<div class="container">
+		 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+		 data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+		 aria-label="Toggle navigation">
+		 <span class="navbar-toggler-icon"></span>
+		 </button>
+		<a class="navbar-brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		 @if (has_nav_menu('primary_navigation'))
+		 {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'navbar-nav nav pull-right']) !!}
+		 @endif
+		 </div>
+	 </div>
  </nav>
- </div>
 </header>
