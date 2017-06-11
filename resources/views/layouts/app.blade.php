8bd4c96979690dfb34d74273bfb9ec066775bf2f
Jasper Frumau <jasper@imagewize.com>
2017-06-11 07:31:19 +0300

    added possible inclusion slider

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,11 @@
   @include('partials.head')
   <body @php(body_class())>
     @php(do_action('get_header'))
+    {{-- @if(is_front_page())
+    @include('partials.header-frontpage')
+    @else
+    @include('partials.header')
+    @endif --}}
     @include('partials.header')
     <div class="wrap container" role="document">
       <div class="content">
