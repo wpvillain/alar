f73ff882d3ed6c34a24411779ac85cba5bcf1937
Jasper Frumau <jasper@imagewize.com>
2019-10-12 09:39:38 +0700

    single new banner image, sidebar

--- app/filters.php
+++ app/filters.php
@@ -128,3 +128,16 @@ add_action( 'init', function() {
 	) );
 
 } );
+
+add_filter('sage/display_sidebar', function ($display) {
+    static $display;
+
+    isset($display) || $display = in_array(true, [
+      // The sidebar will be displayed if any of the following return true
+      is_single(),
+      is_404(),
+    //   is_page_template('template-custom.php')
+    ]);
+
+    return $display;
+});
--- resources/views/layouts/app-single.blade.php
+++ resources/views/layouts/app-single.blade.php
@@ -6,17 +6,17 @@
     @php do_action('get_header') @endphp
     @include('partials.header-single')
     <div class="wrap container" role="document">
-      <div class="content">
-        <main class="main">
-          @yield('content')
-        </main>
-        @if (App\display_sidebar())
-          <aside class="sidebar">
-            @include('partials.sidebar')
-          </aside>
-        @endif
+        <div class="row">
+          <main class="main col-sm">
+            @yield('content')
+          </main>
+          @if (App\display_sidebar())
+            <aside class="sidebar col-sm">
+              @include('partials.sidebar')
+            </aside>
+          @endif
+        </div>
       </div>
-    </div>
     @php do_action('get_footer') @endphp
     @include('partials.footer')
     @php wp_footer() @endphp
