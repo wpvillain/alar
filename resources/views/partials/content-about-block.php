18b4127651bb137714051868081d50b6b23c8fca
Jasper Frumau <jasper@imagewize.com>
2019-10-29 10:30:56 +0700

    about & services blocks tweaks

--- resources/views/partials/content-about-block.php
+++ resources/views/partials/content-about-block.php
@@ -4,7 +4,7 @@
     <div class="row h-100 align-items-center">
       <div class="col-12">
         <h1 class="font-weight-light align-self-center text-center">About</h1>
-        <p class="lead align-self-center">Here at IMWZ Jasper works hard making designers' and entrepreneurs' dreams come true. Jasper builds and manages Laravel &amp; WordPress sites, SEO fine tunes them, gives advices on WordPress as well as Laravel, web design and e-commerce and deals with all things web tech with the help of partners worldwide. Imagewize is a distributed company with a global team, much like Automattic is.</p>
+        <p class="text-center"><?php the_field('about_block'); ?></p>
       </div>
     </div>
   </div>
