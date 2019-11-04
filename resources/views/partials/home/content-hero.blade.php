2cfca6106b1e9f49f6858edffa21f498cfc63f7c
Jasper Frumau <jasper@imagewize.com>
2019-11-04 12:22:28 +0700

    home header tweaks

--- resources/views/partials/home/content-hero.blade.php
+++ resources/views/partials/home/content-hero.blade.php
@@ -3,8 +3,9 @@
   <div class="container h-100">
     <div class="row h-100  align-items-center">
       <div class="col-lg-6">
-        <h1 class="font-weight-light text-lg-left"><?php the_field('hero_header'); ?></h1>
-        <h3 class="text-lg-left font-weight-light"><?php the_field('hero_sub_header'); ?></h2>
+        <h2 class="font-weight-light text-lg-left"><?php the_field('hero_header'); ?></h1>
+        <h3 class="text-lg-left font-weight-lighter"><?php the_field('hero_sub_header'); ?></h2>
+        <h4 class="text-lg-left font-weight-lighter"><?php the_field('hero_sub_sub_header'); ?></h2>
         
       </div>
       <div class="col-lg-6 align-items-center d-flex">
