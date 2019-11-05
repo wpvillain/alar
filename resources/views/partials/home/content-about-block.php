1ac5376325ddc1b83ef3440ebda6c9f8b343653c
Jasper Frumau <jasper@imagewize.com>
2019-11-05 16:00:21 +0700

    about block mobile fix

--- resources/views/partials/home/content-about-block.php
+++ resources/views/partials/home/content-about-block.php
@@ -2,12 +2,12 @@
 <header class="about-masthead">
   <div class="container h-100">
     <div class="row h-100 align-items-center">
-      <div class="col-sm-2"></div>
-      <div class="col-sm-8">
+      <div class="col-lg-2"></div>
+      <div class="col-lg-8">
         <h3 class="font-weight-light align-self-center text-center">About</h3>
         <p class="text-center"><?php the_field('about_block'); ?></p>
       </div>
-      <div class="col-sm-2"></div>
+      <div class="col-lg-2"></div>
     </div>
   </div>
 </header>
