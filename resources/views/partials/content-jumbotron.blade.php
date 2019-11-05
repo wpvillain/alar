97fef9f1aa319b72d0f6fb1fbc8e89752189f7f3
Jasper Frumau <jasper@imagewize.com>
2019-11-05 13:57:18 +0700

    jumbotron image replacement, services content addition

--- resources/views/partials/content-jumbotron.blade.php
+++ resources/views/partials/content-jumbotron.blade.php
@@ -7,13 +7,13 @@
           <h1 class="display-4">The fast & thorough way to <span class="highlight-word">speed up WordPress</span></h1>
   <p class="lead">Optimizin will speed optimize your WordPress site and improve SEO to boot</p>
         <span class="text-center d-inline-block">
-          <a class="btn btn-primary btn-lg w-100" href="#" role="button">Try it free</a>
-        <p class="text-muted">No credit card required</p>
+          <a class="btn btn-primary btn-lg w-100" href="contact" role="button">Free Site Audit</a>
+        {{-- <p class="text-muted">No credit card required</p> --}}
         </span>
         
       </div>
       <div class="col-lg-4 align-items-center d-flex">
-        <img src="https://dummyimage.com/800x600/949494/fff.png" alt="" class="img-fluid">
+        <img src="@asset('images/fast-loading.svg')" alt="" class="img-fluid">
       </div>
     </div>
 
---
+++ resources/views/partials/home/content-services-block.blade.php
@@ -0,0 +1,47 @@
+<!-- Full Page Image Header with Vertically Centered Content -->
+<header class="services-masthead">
+  <div class="container h-100">
+    <div class="row h-100 align-items-center">
+      <div class="col-12 text-center">
+        <h3 class="font-weight-light pt-2"><?php the_field('services_header'); ?></h3>
+        <p class="lead"><?php the_field('services_intro'); ?></p>
+      </div>
+      <div class="col-lg-4">
+      <h4 class="font-weight-light">Minifications & Concatentation</h4>
+        <p>All CSS and JS will be concatenanted and minified where need be to speed up the load
+          time. Every setup is unique, some require extensive concatenation, others less. Your site
+          will get the care it needs.
+        </p>
+      </div>
+      <div class="col-lg-4">
+      <h4 class="font-weight-light">Plugin Pruning</h4>
+        <p> Sometimes the amount of plugins got out of control along the way. Sometimes the plugins
+          used are not the right ones for the job. We will analyse all in use and remove or replace 
+          when need be.
+        </p>
+      </div>
+      <div class="col-lg-4">
+      <h4 class="font-weight-light">Gzip Compression</h4>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
+          Aenean massa. Cum sociis natoque penatibus et</p>
+      </div>
+      <div class="col-lg-4">
+        <h4 class="font-weight-light">Image Optimization</h4>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
+            Aenean massa. Cum sociis natoque penatibus et</p>
+        </div>
+        <div class="col-lg-4">
+          <h4 class="font-weight-light">Unused CSS Cleanup</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
+              Aenean massa. Cum sociis natoque penatibus et</p>
+          </div>
+          <div class="col-lg-4">
+            <h4 class="font-weight-light">Database Cleanup</h4>
+              <p>We will optimize the datbase, make sure all is indexed well and that all redundant data is 
+                removed.
+              </p>
+            </div>
+    </div>
+  </div>
+</header>
+
