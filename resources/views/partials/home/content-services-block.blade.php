e56a41e1d6053cb91cf5467baf26e2963d5775eb
Jasper Frumau <jasper@imagewize.com>
2019-11-05 14:05:26 +0700

    more temporary services text blocks

--- resources/views/partials/home/content-services-block.blade.php
+++ resources/views/partials/home/content-services-block.blade.php
@@ -3,7 +3,7 @@
   <div class="container h-100">
     <div class="row h-100 align-items-center">
       <div class="col-12 text-center">
-        <h3 class="font-weight-light pt-2"><?php the_field('services_header'); ?></h3>
+        <h3 class="font-weight-light pt-4"><?php the_field('services_header'); ?></h3>
         <p class="lead"><?php the_field('services_intro'); ?></p>
       </div>
       <div class="col-lg-4">
@@ -15,25 +15,29 @@
       </div>
       <div class="col-lg-4">
       <h4 class="font-weight-light">Plugin Pruning</h4>
-        <p> Sometimes the amount of plugins got out of control along the way. Sometimes the plugins
+        <p> Sometimes the amount of plugins gets out of control along the way. Sometimes the plugins
           used are not the right ones for the job. We will analyse all in use and remove or replace 
           when need be.
         </p>
       </div>
       <div class="col-lg-4">
-      <h4 class="font-weight-light">Gzip Compression</h4>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
-          Aenean massa. Cum sociis natoque penatibus et</p>
+      <h4 class="font-weight-light">Gzip Compression & File Caching</h4>
+        <p>With the proper server side configuration you can make sure all files that can be compressed
+          will be compressed. You can also make sure the browser does not request the same files over and
+          over using smart caching.
+        </p>
       </div>
       <div class="col-lg-4">
         <h4 class="font-weight-light">Image Optimization</h4>
-          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
-            Aenean massa. Cum sociis natoque penatibus et</p>
+          <p>Images when not uploaded optimized at the right size and dimensions can slow down
+            the website dramatically. We make sure your images are optimized. They will look good,
+            but not grind your site down to a halt.
+          </p>
         </div>
         <div class="col-lg-4">
           <h4 class="font-weight-light">Unused CSS Cleanup</h4>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. 
-              Aenean massa. Cum sociis natoque penatibus et</p>
+            <p>Many themes add loads of styling with CSSS only to use a tiny bit of it. We remove or prune 
+              unused CSS using various tools/p>
           </div>
           <div class="col-lg-4">
             <h4 class="font-weight-light">Database Cleanup</h4>
