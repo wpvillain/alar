15847ce22fc9c053fa13a1a1d857e412d81bb110
Jasper Frumau <jasper@imagewize.com>
2019-11-10 15:00:26 +0700

    post title outside the loop

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -3,7 +3,13 @@
   <div class="container h-100 mb-3">
     <div class="row h-100 align-items-center">
       <div class="col-12 text-center">
-        <h2 class="font-weight-light">{!! App::title() !!}</h2>
+        <h2 class="font-weight-light">
+          @if(is_single())
+          {!! single_post_title() !!}
+          @else
+          {!! App::title() !!}
+          @endif
+        </h2>
       </div>
     </div>
   </div>
