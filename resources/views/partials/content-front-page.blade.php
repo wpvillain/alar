85d569a92017c494197fb480c8ca57bbf1a8087b
Jasper Frumau <jasper@imagewize.com>
2019-12-20 11:08:22 +0700

    remove highlight

--- resources/views/partials/content-front-page.blade.php
+++ resources/views/partials/content-front-page.blade.php
@@ -55,7 +55,7 @@
   <div class="container text-center text-lg-left">
       <div class="row">
         <div class="col-lg-8">
-            <h2 class="display-5 font-weight-light">The fast & thorough way to <span class="highlight-word">speed up WordPress</span></h2>
+            <h2 class="display-5 font-weight-light">The fast & thorough way to speed up WordPress</h2>
             <p class="lead">Optimizin will speed optimize your WordPress site and improve SEO to boot</p>
             <span class="text-center d-inline-block">
             <a class="btn btn-primary btn-lg w-100" href="contact" role="button">Free Site Audit</a>
